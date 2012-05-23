e6f2f1fdbaf5e92cc615d38005bb4a247fb86000
Łukasz Dubiel <bambucha14@gmail.com>
2012-05-23 17:24:23 +0200

    Dodanie pól oraz getterow i setterow do Player.a Dodanie wątku odbierającego komunikaty od gracza. Implementacja trwa.

--- server/src/main/java/pl/vlo/biojpks/server/Player.java
+++ server/src/main/java/pl/vlo/biojpks/server/Player.java
@@ -5,6 +5,25 @@ public class Player
     private String nick;
     private int score;
     
-    
-    
+	public Player(String nick, int score) {
+		super();
+		this.nick = nick;
+		this.score = score;
+	}
+	
+	public String getNick() {
+		return nick;
+	}
+	
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
 }
--- server/src/main/java/pl/vlo/biojpks/server/PlayerThread.java
+++ server/src/main/java/pl/vlo/biojpks/server/PlayerThread.java
@@ -1,5 +1,7 @@
 package pl.vlo.biojpks.server;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,11 +11,31 @@ public class PlayerThread extends Thread
     private Socket socket;
     private Scanner scanner;
     private Writer writer;
+    private Player player;
+    private boolean isConnect;
     
 
     public PlayerThread(Socket socket)
     {
         this.socket = socket;
+        try 
+        {
+			scanner = new Scanner(socket.getInputStream());
+			writer = new OutputStreamWriter(socket.getOutputStream());
+		}
+        catch (IOException e)
+        {
+			e.printStackTrace();
+		}
     }
-    
+
+
+	@Override
+	public void run()
+	{
+		while(isConnect)
+		{
+			
+		}
+	}
 }
